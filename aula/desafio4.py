11e423cadf37720aa8e88a9a5b55b15129ea874b
Carsicklamp255 <160789738+Carsicklamp255@users.noreply.github.com>
2024-09-23 08:47:42 -0300

    Update desafio4.py

--- aula/desafio4.py
+++ aula/desafio4.py
@@ -5,7 +5,8 @@ tamanho máximo de uma subtring que não possui caracteres repetidos.
 'abacdab' --> 4
 'aweawaec' --> 4
 'bbbb' --> 1
-
+usar uma lista para armazenar e depois fazer com que ela limpe a si mesma
+as letras iguais devem ser dadas em digitos númericos
 '''
 
 def tamanho_maximo(string):
